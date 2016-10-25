ebbe2ad8ecd82662a7449e6fa4542739ae75fc4b
Oscar Pérez <mr.omiguelperez@gmail.com>
2016-10-25 11:25:50 -0500

    Agregar el proyecto con todos los archivos

---
+++ BLL/ClienteBLL.cs
@@ -0,0 +1,44 @@
+﻿using DAL;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class ClienteBLL
+    {
+        public int Insertar(ClienteDTO cliente)
+        {
+            using(Contexto db = new Contexto())
+            {
+                Cliente c = new Cliente();
+                c.ClienteId = cliente.ClienteId;
+                c.Direccion = cliente.Direccion;
+                c.Nombre = cliente.Nombre;
+                c.Telefono = cliente.Telefono;
+                db.Clientes.Add(c);
+                return db.SaveChanges();
+            }
+        }
+
+        public List<ClienteDTO> GetRecords()
+        {
+            using(Contexto db = new Contexto())
+            {
+                return db.Clientes
+                    .Select(t => 
+                        new ClienteDTO 
+                        {
+                            ClienteId = t.ClienteId,
+                            Nombre = t.Nombre,
+                            Direccion = t.Direccion,
+                            Telefono = t.Telefono
+                        }
+                    ).ToList();
+            }
+        }
+    }
+}
---
+++ PPTConsola/Program.cs
@@ -0,0 +1,36 @@
+﻿using Entities;
+using BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPTConsola
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ClienteDTO cliente1 = new ClienteDTO
+            {
+                Nombre = "Oscar",
+                Direccion = "San Joaquin",
+                Telefono = "3113931898"
+            };
+
+            ClienteBLL clienteBLL = new ClienteBLL();
+            int nr = clienteBLL.Insertar(cliente1);
+
+            Console.WriteLine("se realzo la operacion; Numero de registros afectados " + nr);
+            Console.ReadKey();
+            Console.WriteLine("Consulta de datos");
+            
+            foreach (var item in clienteBLL.GetRecords())
+            {
+                Console.WriteLine(item.Nombre + " " + item.Telefono);
+            }
+            Console.ReadKey();
+        }
+    }
+}
